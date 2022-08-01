84620577ffb59a3a4b4827601dce47b712877074
吃苹果的猫 <jiaoyaning1210@163.com>
2022-08-01 23:00:51 +0800

    修改aliyun依赖

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,20 +4,21 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.6.10"
-    id("org.jetbrains.compose") version "1.1.0"
+    id("org.jetbrains.compose") version "1.1.1"
 }
 
 group = "com.jyn.kotlin"
 version = "1.0.0"
 
 repositories {
-    maven ("https://maven.aliyun.com/repository/google")
-    maven ("https://maven.aliyun.com/repository/central")
+    maven("https://maven.aliyun.com/repository/google")
+    maven("https://maven.aliyun.com/repository/central")
     maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
 }
 
 dependencies {
     implementation(compose.desktop.currentOs)
+    compileOnly("org.jetbrains.compose:compose-gradle-plugin:1.1.1")
 }
 
 tasks.withType<KotlinCompile> {
