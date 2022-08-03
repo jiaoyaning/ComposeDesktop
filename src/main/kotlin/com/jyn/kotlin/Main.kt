440cfceaa83156d98a2db3c7f9c3e11ea1af00ef
jiaoyaning <jiaoyaning@wps.cn>
2022-08-03 19:34:37 +0800

    feat(导航): 添加切换后的页面效果

--- src/main/kotlin/com/jyn/kotlin/Main.kt
+++ src/main/kotlin/com/jyn/kotlin/Main.kt
@@ -3,6 +3,7 @@ package com.jyn.kotlin
 
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.*
@@ -18,6 +19,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
@@ -33,34 +35,46 @@ fun App() {
 
 
     MaterialTheme {
-        BottomNavigation(
-            backgroundColor = Color.White
-        ) {
-            items.forEachIndexed { index, s ->
-                Column(
-                    modifier = Modifier
-                        .fillMaxHeight()
-                        .weight(1f)
-                        .clickable(
-                            onClick = {
-                                selectedItem = index
-                            },
-                            indication = null,
-                            interactionSource = MutableInteractionSource()
-                        ),
-                    verticalArrangement = Arrangement.Center,
-                    horizontalAlignment = Alignment.CenterHorizontally
-                ) {
-                    Text(items[index])
-                    Spacer(Modifier.padding(top = 2.dp))
-                    AnimatedVisibility(visible = index == selectedItem) {
-                        Surface(
-                            shape = CircleShape,
-                            modifier = Modifier.size(5.dp),
-                            color = Color.Red
-                        ) { }
+        Column {
+            BottomNavigation(
+                backgroundColor = Color.White,
+                elevation = 5.dp
+            ) {
+                items.forEachIndexed { index, s ->
+                    Column(
+                        modifier = Modifier
+                            .fillMaxHeight()
+                            .weight(1f)
+                            .clickable(
+                                onClick = {
+                                    selectedItem = index
+                                },
+                                indication = null,
+                                interactionSource = MutableInteractionSource()
+                            ),
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Text(items[index])
+                        Spacer(Modifier.padding(top = 2.dp))
+                        AnimatedVisibility(visible = index == selectedItem) {
+                            Surface(
+                                shape = CircleShape,
+                                modifier = Modifier.size(5.dp),
+                                color = Color.Red
+                            ) { }
+                        }
                     }
                 }
+            }
+            Box(
+                modifier = Modifier.fillMaxHeight().fillMaxWidth(),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(
+                    text = items[selectedItem],
+                    textAlign = TextAlign.Center
+                )
             }
         }
     }
